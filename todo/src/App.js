28b8157a06a1a7023f3ca9c0b8fc4abd35a56671
DwayneDevelops <mrsuccessfuldev@gmail.com>
2019-04-23 17:59:14 -0500

    problem with handleChange

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Todos from './components/todos';
+
+import TodoList from './components/todoList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-      <Todos />
+      <TodoList />
     </div>
   );
 }
---
+++ todo/src/components/todoList.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { toggleComplete, addNewTodo } from '../actions';
+
+class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      newTodo: ""
+    }
+  }
+
+  handleChange = event => {
+    this.setState({ todos: event.target.value });
+  }
+
+  addTodo = e => {
+    e.preventDefault();
+    this.props.addNewTodo(this.state.newTodo);
+    this.setState({ newTodo: "" });
+  }
+
+  toggleComplete = (e, index) => {
+    e.preventDefault();
+    this.props.toggleComplete(index);
+  }
+
+  render(){
+    return (
+      <div>
+        {this.props.todos.map((todo, index) => (
+          <h4 onClick={e => this.toggleComplete(e, index)} key={index}>
+          {todo.value} {todo.completed && <i className="fas fa-dragon" />}
+          </h4>))
+        }
+        <input
+          type="text"
+          value={this.state.newTodo}
+          placeholder="Add Todo..."
+          onChange={this.handleChange}
+        />
+        <button onClick={this.addTodo}>Add Todo</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  };
+};
+
+export default connect( mapStateToProps, { addNewTodo, toggleComplete })(TodoList);
